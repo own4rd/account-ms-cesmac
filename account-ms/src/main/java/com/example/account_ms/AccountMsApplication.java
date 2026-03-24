a17edf8f65470ec9d2c1116f0cf235fba3dfcf6b
Wagner Palmeira <wagner.araujo@itec.al.gov.br>
2026-03-24 14:05:34 -0300

    Fix: adding description swagger

--- account-ms/src/main/java/com/example/account_ms/AccountMsApplication.java
+++ account-ms/src/main/java/com/example/account_ms/AccountMsApplication.java
@@ -1,8 +1,25 @@
 package com.example.account_ms;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Author Name",
+						email = "author@email.com",
+						url = "https://www.site.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation()
+)
 @SpringBootApplication
 public class AccountMsApplication {
 
